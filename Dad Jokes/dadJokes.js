752ecd5fc8a5c575c5e46111267f019191db8602
Begotten-Traveler <solomondev002@gmail.com>
2025-06-17 22:52:07 +0100

    add: Javascript added

---
+++ Dad Jokes/dadJokes.js
@@ -0,0 +1,20 @@
+let jokeDisplayElement = document.querySelector("#joke");
+let jokeButtonElement = document.querySelector("#jokeBtn");
+
+jokeButtonElement.addEventListener('click', generateJokes);
+
+
+// Async/Await
+async function generateJokes() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json();
+
+    jokeDisplayElement.textContent = data.joke;
+};
+
+generateJokes();
