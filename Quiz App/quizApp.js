3944a58dd2fd239d1ee3846cd99ad9f900157b1a
Begotten-Traveler <solomondev002@gmail.com>
2025-06-18 23:30:23 +0100

    chore: Array of questions added

--- Quiz App/quizApp.js
+++ Quiz App/quizApp.js
@@ -6,3 +6,39 @@ const b_textElement = document.getElementById("b_text");
 const c_textElement = document.getElementById("c_text");
 const d_textElement = document.getElementById("d_text");
 const submitButtonElement = document.getElementById("submit");
+
+// Array of questions
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
