a2c641f48e03d0748f1c299bc391989ad0f23c71
Begotten-Traveler <solomondev002@gmail.com>
2025-06-22 23:22:07 +0100

    chore: styles arrangements and comments

--- Navigation Menu/navigationMenu.js
+++ Navigation Menu/navigationMenu.js
@@ -1,7 +1,7 @@
 const menuIconElement = document.querySelector(".menu-icon");
 const NavMenuElement = document.querySelector(".navmenu");
 
-
+// NavMenu On click event
 menuIconElement.addEventListener('click', () => {
     NavMenuElement.classList.toggle('active');
 });
