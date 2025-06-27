b2efca12f00609dc43b4a3284316fdf0dfba21b4
Begotten-Traveler <solomondev002@gmail.com>
2025-06-27 19:05:22 +0100

    chore: menu icon added

--- Landing Page/landingpage.js
+++ Landing Page/landingpage.js
@@ -1,1 +1,8 @@
-console.log("Yo! I am linked");
+const menuIconsElement = document.getElementById("menu-icon");
+const NavMenuElement = document.querySelector(".navmenu");
+
+// NavMenu On click event
+menuIconsElement.addEventListener('click', () => {
+    NavMenuElement.classList.toggle('active');
+    menuIconsElement.classList.toggle('open');
+});
